6b11d8b67444860979e0c0776ff20668342615a8
hanams67 <142435256+hanams67@users.noreply.github.com>
2024-01-15 17:09:06 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+function Move_forward () {
+    let index = 0
+    VAR = index
+}
+let VAR = 0
+VAR = 1167652
+Move_forward()
+Move_forward
+basic.forever(function () {
+	
+})
+MelodyOptions.Once
+  ///. code script 77615 (null) set full 
